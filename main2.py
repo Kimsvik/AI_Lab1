971ce1a97db9bdaec0a84606859115e6d99d5459
Alexandr <AAKopysov@gmail.com>
2023-10-18 18:40:12 +0300

    пункт 3

--- main2.py
+++ main2.py
@@ -4,10 +4,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import GridSearchCV
-from sklearn import metrics
-from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
 
 import matplotlib.pyplot as plt
 
@@ -25,9 +23,11 @@ import matplotlib.pyplot as plt
 10. concave points (вогнутые точки) - среднее число вогнутых точек в контуре
 """
 
+"""Пункт 2"""
+
 raw_data = pd.read_csv('input/breast_cancer.csv')
 dataset = pd.get_dummies(raw_data, columns=['diagnosis'], drop_first=True)
-print(dataset)
+# print(dataset)
 
 y = dataset['diagnosis_M']
 x = dataset.drop('diagnosis_M', axis=1)
@@ -40,30 +40,21 @@ scaled_x = scaler.transform(x)
 
 scaled_data = pd.DataFrame(scaled_x, columns=x.columns)
 
-# x_train, x_test, y_train, y_test = train_test_split(scaled_data, y, test_size=0.2, random_state=1234)
 x_train, x_test, y_train, y_test = train_test_split(scaled_data, y, test_size=0.2)
 
-# print(x_test)
 
-# y.replace(('M', 'B'), (1, 0), inplace=True)
+# model = KNeighborsClassifier(n_neighbors=5)
+# # print(model)
+# model.fit(x_train, y_train)
+#
+# predictions = model.predict(x_test)
+#
+# # print(classification_report(y_test, predictions))
+# accuracy = accuracy_score(y_test, predictions)
 
-
-# for i in range(1, 51, 1):
-
-
-model = KNeighborsClassifier(n_neighbors=5)
-# print(model)
-model.fit(x_train, y_train)
-
-predictions = model.predict(x_test)
-
-# print(classification_report(y_test, predictions))
-accuracy = accuracy_score(y_test, predictions)
-print(accuracy)
 
 accuracy = []
 number_of_neighbours = 50
-# neighbours = np.arange(1, number_of_neighbours+1, 1)
 
 max_accuracy = 0
 number_of_neighbour = 0
@@ -86,4 +77,16 @@ plt.xlabel('Number of neighbours')
 plt.ylabel('Accuracy')
 plt.show()
 
+"""Пункт 3"""
+n_splits = 5
+kf = KFold(n_splits=n_splits, shuffle=True)
+
+array_of_quality = []
+
+for train_index, test_index in kf.split(scaled_data):
+    X_train, X_test = scaled_data[train_index], scaled_data[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+    # обучаем модель и оцениваем на тестовом наборе данных
+
+    array_of_quality.append(cross_val_score(model, X, y, cv=kf, scoring='accuracy'))
 
