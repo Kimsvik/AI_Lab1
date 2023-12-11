ac63c7cb3413dc8dcc6adc135731a05f7e444134
Sergey <serg2000kim@gmail.com>
2023-12-12 00:01:33 +0300

    Lab3 - 1 Serega's commit

--- Lab3.py
+++ Lab3.py
@@ -1,3 +1,31 @@
-def p1_read(path, encoding):
-    data_file = pd.read_csv(path, encoding=encoding)
+import pandas as pd
+import matplotlib.pyplot as plt
+from collections import Counter
+import numpy as np
+from sklearn import feature_extraction
+from sklearn import model_selection
+from sklearn.naive_bayes import MultinomialNB
+from sklearn import metrics
+from sklearn.metrics import confusion_matrix
+from sklearn.svm import SVC
+
+import seaborn as sns
+
+"ПУНКТ 1"
+def p1_read(path):
+    data_file = pd.read_csv(path)
     return data_file
+
+
+"ПУНКТ 2"
+def p2_sum(data_n):
+    data_n.isnull().sum()
+    data_n['workclass'].hist()
+    f, ax = plt.subplots(figsize=(10, 8))
+    ax = sns.countplot(x="income", hue="sex", data=data, palette="Set1")
+    ax.set_title("Frequency distribution of income variable wrt sex")
+    plt.show
+
+
+data = p1_read('input/income.csv')
+p2_sum(data)
