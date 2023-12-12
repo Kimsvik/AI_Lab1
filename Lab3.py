7a917bed2788bd3e2cbb96e1529222c3917d637b
Sergey <serg2000kim@gmail.com>
2023-12-12 14:17:02 +0300

    Lab3 - 2 Serega's commit

--- Lab3.py
+++ Lab3.py
@@ -8,8 +8,9 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn import metrics
 from sklearn.metrics import confusion_matrix
 from sklearn.svm import SVC
-
 import seaborn as sns
+from sklearn.tree import DecisionTreeClassifier
+
 
 "ПУНКТ 1"
 def p1_read(path):
@@ -26,9 +27,47 @@ def p2_sum(data_n):
     ax.set_title("Frequency distribution of income variable wrt sex")
     plt.show()
 
+    f, ax = plt.subplots(figsize=(10, 8))
+    x = data_n['age']
+    ax = sns.distplot(x, bins=10, color='blue')
+    ax.set_title("Distribution of age variable")
+    plt.show()
 
-#data = p1_read('input/income.csv')
-#p2_sum(data)
+    f, ax = plt.subplots(figsize=(10, 8))
+    x = data_n['age']
+    ax = sns.boxplot(x)
+    ax.set_title("Visualize outliers in age variable")
+    plt.show()
+
+    f, ax = plt.subplots(figsize=(10, 8))
+    ax = sns.boxplot(x="income", y="age", data=data_n)
+    ax.set_title("Visualize income wrt age variable")
+    plt.show()
+
+    f, ax = plt.subplots(figsize=(10, 8))
+    ax = sns.boxplot(x="income", y="age", hue="sex", data=data_n)
+    ax.set_title("Visualize income wrt age and sex variable")
+    ax.legend(loc='upper right')
+    plt.show()
+
+    data_n.replace('?', np.NaN, inplace=True)
+
+    categorical = [var for var in data_n.columns if data[var].dtype=='O']
+    data_n[categorical].head()
 
 
-raw_data = pd.read_csv('input/breast_cancer.csv')
+"ПУНКТ 3"
+def p3_split(data_file):
+    data_file['income'] = data_file['income'].map({'>50K': 1, '<=50K': 0})
+    x_train, x_test, y_train, y_test = model_selection.train_test_split(X, data_file['income'], test_size=0.33)
+    return x_train, x_test, y_train, y_test
+
+"ПУНКТ 4"
+
+
+
+data = p1_read('C:/Users/Сергей/PycharmProjects/AI_Lab1/input/income.csv')
+p2_sum(data)
+
+
+
